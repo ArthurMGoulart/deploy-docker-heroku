46517f2e3c3db2f56ab1c8dce3fed751ae158d73
ArthurMGoulart <budiarthur@gmail.com>
2022-02-10 15:43:09 -0300

    Eslint branch

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send(`<p> Executando a porta: ${port} </p>`));
+app.get('/', (req, res) => res.send(`<p> Executando a porta: ${port} </p> <h1> Usando Eslint! </h1>`));
 
 app.listen(port, () => console.log(`Online na porta ${port}`));
